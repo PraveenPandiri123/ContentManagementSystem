76c3f3fb33e77d6c597e2d77a5a62c9a10bf4e92
PraveenPandiri123 <praveenpandiri171@gmail.com>
2024-03-29 11:02:36 +0530

    findbyid,update

--- src/main/java/com/example/cms/controller/BlogController.java
+++ src/main/java/com/example/cms/controller/BlogController.java
@@ -1,11 +1,15 @@
 package com.example.cms.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.cms.entity.Blog;
+import com.example.cms.repository.BlogRepository;
 import com.example.cms.requestdto.BlogRequest;
 import com.example.cms.responsedto.BlogResonse;
 import com.example.cms.service.BlogService;
@@ -22,5 +26,19 @@ public class BlogController {
 	@PostMapping("/users/{userId}/blogs")
 	public ResponseEntity<ResponseStructure<BlogResonse>> createBlog(@PathVariable int userId, @RequestBody @Valid BlogRequest blogRequest) {
 		return blogService.createBlog(userId, blogRequest);
+	}
+	@GetMapping("/blogs/{blogId}")
+	public ResponseEntity<ResponseStructure<BlogResonse>> findByBlogId(@PathVariable int blogId)
+	{
+		return blogService.findByBlogId(blogId);
+	}
+	@PutMapping("/blogs/{blogId}")
+	public ResponseEntity<ResponseStructure<BlogResonse>> updateBlog(@RequestBody
+			@Valid BlogRequest blogRequest,@PathVariable int blogId){
+		return blogService.updateBlog(blogId,blogRequest);
+	}
+	@GetMapping("/titles/{title}/blogs")
+	public ResponseEntity<Boolean> checkForBlogTitleAvailability(@PathVariable String title){
+		return blogService.checkForBlogTitleAvailability(title);
 	}
 }
